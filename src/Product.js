474e853149a278a7d4ffb34e34b29677096b818f
raylu <90059+raylu@users.noreply.github.com>
2023-06-28 22:45:48 -0700

    addItemToCart: don't get product twice

--- src/Product.js
+++ src/Product.js
@@ -2,19 +2,19 @@ import "./styles/Product.css";
 
 function Product(props) {
     const addItemToCart = (event) => {
-        let selectedItem = event.target.value;
+        let product = event.target.value;
         for (let i = 0; i < props.cartItems.length; i++) {
-            if (props.cartItems[i]["product"] === selectedItem) {
-                let newCart = [...props.cartItems];
+            if (props.cartItems[i]["product"] === product) {
+                const newCart = [...props.cartItems];
                 newCart[i] = {
-                    product: selectedItem,
+                    product: product,
                     quantity: props.cartItems[i]["quantity"] + 1,
                 }
                 props.setCartItems(newCart);
                 return;
             }
         }
-        props.setCartItems(cartItems => [...cartItems, {product: event.target.value, quantity: 1}]);
+        props.setCartItems(cartItems => [...cartItems, {product, quantity: 1}]);
     }
 
     return (
